741ea6541b9eb8cf3d646f5b37387c0a52cbd06a
Zakhar <zax_2000@mail.ru>
2025-01-15 17:46:24 +0500

    Рефакторинг SizeParser.
    1. Рефакторинг SizeParser.
    2. Добавлены тесты для SizeParser.

---
+++ TagCloud.Tests/ParsersTests/SizeParserTest.cs
@@ -0,0 +1,88 @@
+﻿using FileSenderRailway;
+using FluentAssertions;
+using System.Drawing;
+using TagCloud.Parsers;
+
+namespace TagCloud.Tests.ParsersTests
+{
+    [TestFixture]
+    internal class SizeParserTest
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null!)]
+        public void ParseImageSize_ThrowsException_WithInvalidInput(string value)
+        {
+            var expected = Result.Fail<Size>($"Некорректная строка {value}");
+            var actual = SizeParser.ParseImageSize(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("100:")]
+        [TestCase(":100")]
+        [TestCase("100:100:100")]
+        [TestCase("abc")]
+        public void ParseImageSize_ThrowsException_WithIncorrectFormat(string value)
+        {
+            var expected = Result.Fail<Size>($"Некорректный формат размера изображения: \"{value}\", используйте формат \"Ширина:Высота\", например 5000:5000");
+            var actual = SizeParser.ParseImageSize(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("abc:100")]
+        [TestCase("100:abc")]
+        [TestCase("abc:abc")]
+        public void ParseImageSize_ThrowsException_WithIncorrectInput(string value)
+        {
+            var expected = Result.Fail<Size>($"Передано не число: abc");
+            var actual = SizeParser.ParseImageSize(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("0:100")]
+        [TestCase("100:0")]
+        [TestCase("0:0")]
+        public void ParseImageSize_ThrowsException_WithInputLessThanZero(string value)
+        {
+            var expected = Result.Fail<Size>($"Переданное числовое значение должно быть больше 0: 0");
+            var actual = SizeParser.ParseImageSize(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null!)]
+        public void ParseSizeDimension_ThrowsException_WithInvalidInputAsString(string value)
+        {
+            var expected = Result.Fail<int>($"Некорректная строка {value}");
+            var actual = SizeParser.ParseSizeDimension(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("abc")]
+        public void ParseSizeDimension_ThrowsException_WithIncorrectInputAsString(string value)
+        {
+            var expected = Result.Fail<int>($"Передано не число: {value}");
+            var actual = SizeParser.ParseSizeDimension(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("0")]
+        [TestCase("-1")]
+        public void ParseSizeDimension_ThrowsException_WithInputLessThanZeroAsString(string value)
+        {
+            var expected = Result.Fail<int>($"Переданное числовое значение должно быть больше 0: {value}");
+            var actual = SizeParser.ParseSizeDimension(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("0")]
+        [TestCase("-1")]
+        public void ParseSizeDimension_ThrowsException_WithInputLessThanZeroAsInt(int value)
+        {
+            var expected = Result.Fail<int>($"Переданное числовое значение должно быть больше 0: {value}");
+            var actual = SizeParser.ParseSizeDimension(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
--- TagCloud/Parsers/SizeParser.cs
+++ TagCloud/Parsers/SizeParser.cs
@@ -7,20 +7,38 @@ namespace TagCloud.Parsers
     {
         public static Result<Size> ParseImageSize(string size)
         {
-            var dimensions = size.Split(':');
-            if (dimensions.Length != 2)
+            if (string.IsNullOrWhiteSpace(size))
             {
-                return Result.Fail<Size>($"Некорректный формат размера изображения: {size}, используйте формат Ширина:Высота, например 5000:5000");
+                return Result.Fail<Size>($"Некорректная строка {size}");
             }
 
-            var width = ParseSizeDimension(dimensions[0]).GetValueOrThrow();
-            var height = ParseSizeDimension(dimensions[1]).GetValueOrThrow();
+            var dimensions = size.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            if (dimensions.Length != 2)
+            {
+                return Result.Fail<Size>($"Некорректный формат размера изображения: \"{size}\", используйте формат \"Ширина:Высота\", например 5000:5000");
+            }
 
-            return new Size(width, height).AsResult();
+            var width = ParseSizeDimension(dimensions[0]);
+            if (!width.IsSuccess)
+            {
+                return Result.Fail<Size>(width.Error);
+            }
+
+            var height = ParseSizeDimension(dimensions[1]);
+            if (!height.IsSuccess)
+            {
+                return Result.Fail<Size>(height.Error);
+            }
+
+            return new Size(width.Value, height.Value).AsResult();
         }
 
         public static Result<int> ParseSizeDimension(string dimension)
         {
+            if (string.IsNullOrWhiteSpace(dimension))
+            {
+                return Result.Fail<int>($"Некорректная строка {dimension}");
+            }
             if (!int.TryParse(dimension, out var result))
             {
                 return Result.Fail<int>($"Передано не число: {dimension}");
