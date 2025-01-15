437d5c41b61be56e9a339b7f359e0e94e0f8cdd3
Zakhar <zax_2000@mail.ru>
2025-01-15 17:13:24 +0500

    Рефакторинг FontParser.
    1. Рефакторинг FontParser.
    2. Добавлены тесты для FontParser.

---
+++ TagCloud.Tests/ParsersTests/FontParserTest.cs
@@ -0,0 +1,40 @@
+﻿using FileSenderRailway;
+using FluentAssertions;
+using System.Drawing;
+using TagCloud.Parsers;
+
+namespace TagCloud.Tests.ParsersTests
+{
+    [TestFixture]
+    internal class FontParserTest
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null!)]
+        public void FontParser_ThrowsException_WithInvalidInput(string value)
+        {
+            var expected = Result.Fail<FontFamily>($"Некорректная строка {value}");
+            var actual = FontParser.ParseFont(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("abc")]
+        public void FontParser_ThrowsException_WithUnknownColor(string value)
+        {
+            var expected = Result.Fail<FontFamily>($"Неизвестный шрифт {value}");
+            var actual = FontParser.ParseFont(value);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase("Arial")]
+        [TestCase("Times New Roman")]
+        [TestCase("Georgia")]
+        [TestCase("Verdana")]
+        public void FontParser_WorksCorrectly(string value)
+        {
+            var expected = new FontFamily(value);
+            var actual = FontParser.ParseFont(value).GetValueOrThrow();
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
--- TagCloud/Parsers/FontParser.cs
+++ TagCloud/Parsers/FontParser.cs
@@ -7,6 +7,10 @@ namespace TagCloud.Parsers
     {
         public static Result<FontFamily> ParseFont(string font)
         {
+            if (string.IsNullOrWhiteSpace(font))
+            {
+                return Result.Fail<FontFamily>($"Некорректная строка {font}");
+            }
             if (!FontFamily.Families.Any(
                 x => x.Name.Equals(font, StringComparison.OrdinalIgnoreCase)))
             {
